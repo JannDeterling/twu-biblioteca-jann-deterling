a94efbb026385ff4b3fc31eb2c6f6b0b633e5204
Jann Deterling <deterling.jann@gmail.com>
2019-09-27 16:19:42 +0200

    [Jann] [1.1] Added Testcase for the WelcomeMessagePrinter

---
+++ test/com/twu/biblioteca/WelcomeMessagePrinterTest.java
@@ -0,0 +1,31 @@
+package com.twu.biblioteca;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class WelcomeMessagePrinterTest {
+
+  private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+
+  @BeforeClass
+  public static void setupTest() {
+    System.setOut(new PrintStream(outContent));
+  }
+
+  @Test
+  public void shouldPrintWelcomeMessageToStdOut() {
+    // Add the lineSeparator because the WelcomeMessagePrinter should print the welcome message to a separate line and
+    // the next content should also start on the next line!
+    String expectedOutcome = "Welcome to Biblioteca. Your one-stop-shop for great book titles in "
+        + "Bangalore!" + System.lineSeparator();
+    WelcomeMessagePrinter.printWelcomeMessage();
+    assertEquals(expectedOutcome, outContent.toString());
+  }
+}
