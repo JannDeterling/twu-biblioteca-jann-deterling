33c82873d275929cf0cb0baba2ac78687ed4c08f
Jann Deterling <deterling.jann@gmail.com>
2019-09-27 16:20:18 +0200

    [Jann] [1.1] Added WelcomeMessagePrinter class

---
+++ src/com/twu/biblioteca/WelcomeMessagePrinter.java
@@ -0,0 +1,12 @@
+package com.twu.biblioteca;
+
+public class WelcomeMessagePrinter {
+
+  private static final String WELCOME_MESSAGE = "Welcome to Biblioteca. Your one-stop-shop for great book titles in "
+      + "Bangalore!";
+
+  public static void printWelcomeMessage(){
+    System.out.println(WELCOME_MESSAGE);
+  }
+
+}
