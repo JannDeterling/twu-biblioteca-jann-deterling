800e04214d58edf7762a307f2118219065366e00
Jann Deterling <deterling.jann@gmail.com>
2019-09-28 16:42:29 +0200

    [Jann] [1.3] Update BookTest to test the new attributes of a Book

--- test/com/twu/biblioteca/books/BookTest.java
+++ test/com/twu/biblioteca/books/BookTest.java
@@ -5,24 +5,48 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
+import java.time.Year;
+
 public class BookTest {
 
   @Test
-  public void shouldCreateValidBook(){
+  public void shouldCreateValidBook() {
     String title = "Domain Driven Design";
-    Book book = new Book(title);
+    String author = "Eric Evans";
+    Year publishedYear = Year.parse("2003");
+    Book book = new Book(title, author, publishedYear);
     assertNotNull(book);
     assertEquals(title, book.getTitle());
   }
 
   @Test(expected = AssertionError.class)
-  public void shouldNotCreateValidBookWithNull(){
-    Book book = new Book(null);
+  public void shouldNotCreateValidBookWithNull() {
+    Book book = new Book(null, null, null);
   }
 
   @Test(expected = AssertionError.class)
-  public void shouldNotCreateValidBookWithEmptyTitle(){
-    Book book = new Book("");
+  public void shouldNotCreateValidBookWithEmptyTitle() {
+    Book book = new Book("", "Eric Evans", Year.parse("2003"));
   }
 
+
+  @Test(expected = AssertionError.class)
+  public void shouldNotCreateValidBookWithNullTitle() {
+    Book book = new Book(null, "Eric Evans", Year.parse("2003"));
+  }
+
+  @Test(expected = AssertionError.class)
+  public void shouldNotCreateValidBookWithEmptyAuthor() {
+    Book book = new Book("Domain Driven Design", "", Year.parse("2003"));
+  }
+
+  @Test(expected = AssertionError.class)
+  public void shouldNotCreateValidBookWithNullAuthor() {
+    Book book = new Book("Domain Driven Design", null, Year.parse("2003"));
+  }
+
+  @Test(expected = AssertionError.class)
+  public void shouldNotCreateValidBookWithNullYear() {
+    Book book = new Book("Domain Driven Design", "Eric Evans", null);
+  }
 }
