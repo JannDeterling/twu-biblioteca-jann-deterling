107a34c74e77bd816674986e7bb1cd320c63983b
Jann Deterling <deterling.jann@gmail.com>
2019-09-28 16:20:45 +0200

    [Jann] [1.2] Add BookService usage to BibliotecaApp with SampleBookRepository

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,14 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.books.BookService;
+import com.twu.biblioteca.books.SampleBookRepository;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         WelcomeMessagePrinter.printWelcomeMessage();
+
+        BookService bookService = new BookService(new SampleBookRepository());
+        bookService.printBookList();
     }
 }
