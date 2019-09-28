89a5d56b2ac7d7907bd78bda99862aa96f0c31ed
Jann Deterling <deterling.jann@gmail.com>
2019-09-28 19:07:29 +0200

    [Jann] [1.5] Add test case to MainMenuTest for the retry option

--- test/com/twu/biblioteca/menu/MainMenuTest.java
+++ test/com/twu/biblioteca/menu/MainMenuTest.java
@@ -18,12 +18,11 @@ import com.twu.biblioteca.books.SampleBookRepository;
 public class MainMenuTest {
 
   private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-  private static final ByteArrayInputStream inContent = new ByteArrayInputStream("1".getBytes());
 
   @BeforeClass
   public static void setupTest() {
     System.setOut(new PrintStream(outContent));
-    System.setIn(inContent);
+
   }
 
   @Test(expected = AssertionError.class)
@@ -48,6 +47,8 @@ public class MainMenuTest {
 
   @Test
   public void shouldDisplayMenu() {
+    final ByteArrayInputStream inContent = new ByteArrayInputStream("1".getBytes());
+    System.setIn(inContent);
     String description = "List Books";
     MainMenu mainMenu = new MainMenu();
     SampleBookRepository sampleBookRepository = new SampleBookRepository();
@@ -58,6 +59,37 @@ public class MainMenuTest {
     StringBuilder stringBuilder = new StringBuilder();
     stringBuilder.append("1) ")
         .append(description)
+        .append(System.lineSeparator())
+        .append("Please select a option (enter the number):")
+        .append(System.lineSeparator());
+
+    List<Book> books = sampleBookRepository.getAllBooks();
+    books.forEach(book -> stringBuilder.append(book.toString()).append(System.lineSeparator()));
+
+    assertNotEquals(outContent.toString(), "");
+    assertEquals(stringBuilder.toString(), outContent.toString());
+  }
+
+  @Test
+  public void shouldDisplayMenuWithRetry() {
+    final ByteArrayInputStream inContent = new ByteArrayInputStream("-1 1".getBytes());
+    System.setIn(inContent);
+    String description = "List Books";
+    MainMenu mainMenu = new MainMenu();
+    SampleBookRepository sampleBookRepository = new SampleBookRepository();
+    BookService bookService = new BookService(sampleBookRepository);
+    mainMenu.registerMenuOption(description, bookService::printBookList);
+    mainMenu.displayMenu();
+
+    StringBuilder stringBuilder = new StringBuilder();
+
+    stringBuilder.append("1) List Books")
+        .append(System.lineSeparator())
+        .append("Please select a option (enter the number):")
+        .append(System.lineSeparator())
+        .append("Please select a valid option!")
+        .append(System.lineSeparator())
+        .append("1) List Books")
         .append(System.lineSeparator())
         .append("Please select a option (enter the number):")
         .append(System.lineSeparator());
