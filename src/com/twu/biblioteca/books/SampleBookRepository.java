24f4bbf211dd11e13e8df9cd94b4429e455ba098
Jann Deterling <deterling.jann@gmail.com>
2019-09-28 16:45:36 +0200

    [Jann] [1.3] Update SampleBookRepository, change static sample data

--- src/com/twu/biblioteca/books/SampleBookRepository.java
+++ src/com/twu/biblioteca/books/SampleBookRepository.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca.books;
 
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,9 +8,9 @@ import java.util.List;
 public class SampleBookRepository implements BookRepository {
 
   private static final List<Book> SAMPLE_BOOK_LIST = new ArrayList<Book>(Arrays.asList(
-      new Book("Refactoring: Improving the Design of Existing Code (2nd Edition)"),
-      new Book("Head First Design Patterns"),
-      new Book("Domain Driven Design")
+      new Book("Refactoring: Improving the Design of Existing Code (2nd Edition)", "Martin Fowler", Year.of(2018)),
+      new Book("Head First Design Patterns","Kathy Sierra, Elisabeth Robson", Year.of(2004)),
+      new Book("Domain Driven Design", "Eric Evans", Year.of(2003))
   ));
 
 
