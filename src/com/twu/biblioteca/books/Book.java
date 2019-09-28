92b210066f853028697404315cd97635d2d207ed
Jann Deterling <deterling.jann@gmail.com>
2019-09-28 16:45:04 +0200

    [Jann] [1.3] Update Book, adds the new attribute author and year

--- src/com/twu/biblioteca/books/Book.java
+++ src/com/twu/biblioteca/books/Book.java
@@ -1,27 +1,55 @@
 package com.twu.biblioteca.books;
 
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
+import java.time.Year;
 
 public class Book {
 
   private String title;
+  private String author;
+  private Year publishedYear;
 
 
-  public Book(String title) {
+  public Book(String title, String author, Year publishedYear) {
+    this.setTitle(title);
+    this.setAuthor(author);
+    this.setPublishedYear(publishedYear);
+  }
+
+  private void setTitle(String title) {
     assert title!= null : "Title should not be null";
     assert !title.equals("") : "Title should not be empty";
     this.title = title;
   }
 
+  private void setAuthor(String author) {
+    assert author!= null : "Author should not be null";
+    assert !author.equals("") : "Author should not be empty";
+    this.author = author;
+  }
+
+  private void setPublishedYear(Year publishedYear) {
+    assert publishedYear!= null : "PublishedYear should not be null";
+    this.publishedYear = publishedYear;
+  }
+
   public String getTitle() {
-    return this.title;
+    return title;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public Year getPublishedYear() {
+    return publishedYear;
   }
 
   @Override
   public String toString() {
     return "Book{" +
         "title='" + title + '\'' +
+        ", author='" + author + '\'' +
+        ", publishedYear=" + publishedYear +
         '}';
   }
 }
