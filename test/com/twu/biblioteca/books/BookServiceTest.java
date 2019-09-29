a8a71b51672336022cc34466543bf5c7cdc44c0c
Jann Deterling <deterling.jann@gmail.com>
2019-09-29 15:05:25 +0200

    [Jann] [1.7] Adds test case to find and finally checkout a book

--- test/com/twu/biblioteca/books/BookServiceTest.java
+++ test/com/twu/biblioteca/books/BookServiceTest.java
@@ -2,33 +2,65 @@ package com.twu.biblioteca.books;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.List;
 
 public class BookServiceTest {
 
-  private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final InputStream systemIn = System.in;
+  private final PrintStream systemOut = System.out;
+  private ByteArrayOutputStream outContent;
 
-  @BeforeClass
-  public static void setupTest() {
+  @Before
+  public void setUpOutput() {
+    outContent = new ByteArrayOutputStream();
     System.setOut(new PrintStream(outContent));
+  }
+
+  @After
+  public void restoreSystemInputOutput() {
+    System.setIn(systemIn);
+    System.setOut(systemOut);
   }
 
   @Test
   public void shouldPrintAllBooksFromTheRepository() {
-    SampleBookRepository sampleBookRepository = new SampleBookRepository();
-    List<Book> books = sampleBookRepository.getAllBooks();
+    final String expectedOutcome = this.expectedPrintedBooks();
+    BookService bookService = new BookService(new SampleBookRepository());
+    bookService.printBookList();
+    assertEquals(outContent.toString(), expectedOutcome);
+  }
+
+  @Test
+  public void shouldCheckoutABook() {
+    final StringBuilder expectedOutcome = new StringBuilder();
+    expectedOutcome.append(this.expectedPrintedBooks());
+    expectedOutcome.append(System.lineSeparator());
+    expectedOutcome.append("Please enter the title of the book you want to check out:");
+    this.provideTestInput("Domain Driven Design");
+    BookService bookService = new BookService(new SampleBookRepository());
+    bookService.checkOutBook();
+
+  }
+
+  private void provideTestInput(String data) {
+    ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+    System.setIn(testIn);
+  }
+
+  private String expectedPrintedBooks() {
+    final SampleBookRepository sampleBookRepository = new SampleBookRepository();
+    final List<Book> books = sampleBookRepository.getAllBooks();
     final StringBuilder stringBuilder = new StringBuilder();
     books.forEach(book -> stringBuilder.append(book.toString()).append(System.lineSeparator()));
-
-    BookService bookService = new BookService(sampleBookRepository);
-    bookService.printBookList();
-
-    assertEquals(outContent.toString(), stringBuilder.toString());
+    return stringBuilder.toString();
   }
 
 }
