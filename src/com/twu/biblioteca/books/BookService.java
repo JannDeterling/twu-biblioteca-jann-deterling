17ba74699913c5c69b6a877be9345404ba741909
Jann Deterling <deterling.jann@gmail.com>
2019-09-29 15:05:49 +0200

    [Jann] [1.7] Adds Method to find and finally check out a book

--- src/com/twu/biblioteca/books/BookService.java
+++ src/com/twu/biblioteca/books/BookService.java
@@ -1,5 +1,8 @@
 package com.twu.biblioteca.books;
 
+import java.util.Optional;
+import java.util.Scanner;
+
 public class BookService {
 
   private BookRepository bookRepository;
@@ -10,6 +13,14 @@ public class BookService {
 
   public void printBookList(){
     this.bookRepository.getAllBooks().forEach(book -> System.out.println(book.toString()));
+  }
+
+  public void checkOutBook() {
+    final Scanner scanner = new Scanner(System.in);
+    this.printBookList();
+    System.out.println("Please enter the title of the book you want to check out:");
+    String title = scanner.next();
+    Optional<Book> optionalBook = this.bookRepository.getBookByTitle(title);
   }
 
 }
