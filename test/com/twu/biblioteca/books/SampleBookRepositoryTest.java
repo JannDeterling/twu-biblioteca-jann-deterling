4ae2f55ceb173c1b8690635f0b0d9e43bf64f86b
Jann Deterling <deterling.jann@gmail.com>
2019-09-29 15:04:11 +0200

    [Jann] [1.7] Adds test case to find a single book by name

--- test/com/twu/biblioteca/books/SampleBookRepositoryTest.java
+++ test/com/twu/biblioteca/books/SampleBookRepositoryTest.java
@@ -1,12 +1,16 @@
 package com.twu.biblioteca.books;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 public class SampleBookRepositoryTest {
 
@@ -20,4 +24,25 @@ public class SampleBookRepositoryTest {
     assertEquals(bookList.size(), 3);
   }
 
+  @Test
+  public void shouldGetBookByTitle(){
+    String title = "Domain Driven Design";
+    SampleBookRepository sampleBookRepository = new SampleBookRepository();
+    Optional<Book> optionalBook = sampleBookRepository.getBookByTitle(title);
+    assertTrue(optionalBook.isPresent());
+    Book book = optionalBook.get();
+    assertNotNull(book);
+    assertEquals(title, book.getTitle());
+  }
+
+  @Test
+  public void shouldNotGetBookByTitle(){
+    String title = "New Domain Driven Design";
+    SampleBookRepository sampleBookRepository = new SampleBookRepository();
+    Optional<Book> optionalBook = sampleBookRepository.getBookByTitle(title);
+    assertFalse(optionalBook.isPresent());
+    Book book = optionalBook.get();
+    assertNull(book);
+
+  }
 }
