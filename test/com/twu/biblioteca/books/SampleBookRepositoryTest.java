b7a7e1fd2c44eeba69d4d4dd2337ca444a0f9197
Jann Deterling <deterling.jann@gmail.com>
2019-09-30 08:30:59 +0200

    [Jann] [1.7] Adds new test case for available books

--- test/com/twu/biblioteca/books/SampleBookRepositoryTest.java
+++ test/com/twu/biblioteca/books/SampleBookRepositoryTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,6 +12,20 @@ import java.util.List;
 import java.util.Optional;
 
 public class SampleBookRepositoryTest {
+
+  @Test
+  public void shouldReturnListOfAllAvailableBooks() {
+    SampleBookRepository sampleBookRepository = new SampleBookRepository();
+    List<Book> bookList = sampleBookRepository.getAllBooksAvailable();
+    assertNotNull(bookList);
+    assertNotEquals(bookList.size(), 0);
+    assertEquals(bookList.size(), 3);
+    bookList.get(0).checkOut();
+    bookList = sampleBookRepository.getAllBooksAvailable();
+    assertNotNull(bookList);
+    assertNotEquals(bookList.size(), 0);
+    assertEquals(bookList.size(), 2);
+  }
 
   @Test
   public void shouldReturnListOfAllBooks() {
