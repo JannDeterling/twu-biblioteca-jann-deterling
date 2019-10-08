1efa5fdb5f84382d968499901b70580677413794
Jann Deterling <jann.deterling@thoughtworks.com>
2019-10-08 15:04:48 +0200

    [Jann] [2.3] Updates tests to expect a logged in user

--- test/com/twu/biblioteca/books/BookServiceTest.java
+++ test/com/twu/biblioteca/books/BookServiceTest.java
@@ -5,6 +5,10 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
+import com.twu.biblioteca.users.LoggedInUserSingleton;
+import com.twu.biblioteca.users.SampleUserRepository;
+import com.twu.biblioteca.users.User;
+import com.twu.biblioteca.users.UserService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,17 +18,25 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.List;
+import java.util.Optional;
 
 public class BookServiceTest {
 
   private final InputStream systemIn = System.in;
   private final PrintStream systemOut = System.out;
   private ByteArrayOutputStream testOutput;
+  private BookService bookService;
+  private SampleBookRepository sampleBookRepository;
 
   @Before
   public void setUpOutput() {
+    this.logInASampleUser();
+    UserService userService = new UserService(new SampleUserRepository());
+    this.sampleBookRepository = new SampleBookRepository();
+    this.bookService = new BookService(sampleBookRepository, userService);
     testOutput = new ByteArrayOutputStream();
     System.setOut(new PrintStream(testOutput));
+
   }
 
   @After
@@ -37,21 +49,18 @@ public class BookServiceTest {
   public void shouldPrintAllBooksFromTheRepository() {
     final SampleBookRepository sampleBookRepository = new SampleBookRepository();
     final String expectedOutcome = this.expectedPrintedBooks(sampleBookRepository.getAll());
-    final BookService bookService = new BookService(sampleBookRepository);
     bookService.printBookList();
     assertThat(expectedOutcome, is(equalTo(testOutput.toString())));
   }
 
   @Test
   public void shouldCheckoutABook() {
-    final SampleBookRepository sampleBookRepository = new SampleBookRepository();
     final StringBuilder expectedOutput = new StringBuilder();
     expectedOutput.append(this.expectedPrintedBooks(sampleBookRepository.getAllAvailable()))
         .append("Please enter the title of the book you want to check out:")
         .append(System.lineSeparator())
         .append("Thank you! Enjoy the book.")
         .append(System.lineSeparator());
-    final BookService bookService = new BookService(sampleBookRepository);
     this.provideTestInput("Domain Driven Design");
     bookService.checkOutBook();
     assertThat(expectedOutput.toString(), is(equalTo(testOutput.toString())));
@@ -59,7 +68,6 @@ public class BookServiceTest {
 
   @Test
   public void shouldNotCheckoutANonExistingBook() {
-    final SampleBookRepository sampleBookRepository = new SampleBookRepository();
     final StringBuilder expectedOutput = new StringBuilder();
     expectedOutput.append(this.expectedPrintedBooks(sampleBookRepository.getAllAvailable()))
         .append("Please enter the title of the book you want to check out:")
@@ -67,14 +75,12 @@ public class BookServiceTest {
         .append("Sorry, that book is not available.")
         .append(System.lineSeparator());
     this.provideTestInput("New Domain Driven Design");
-    final BookService bookService = new BookService(sampleBookRepository);
     bookService.checkOutBook();
     assertThat(expectedOutput.toString(), is(equalTo(testOutput.toString())));
   }
 
   @Test
   public void shouldNotDoubleCheckoutABook() {
-    final SampleBookRepository sampleBookRepository = new SampleBookRepository();
     final StringBuilder expectedOutput = new StringBuilder();
 
     expectedOutput.append(this.expectedPrintedBooks(sampleBookRepository.getAllAvailable()))
@@ -82,7 +88,6 @@ public class BookServiceTest {
         .append(System.lineSeparator())
         .append("Thank you! Enjoy the book.")
         .append(System.lineSeparator());
-    final BookService bookService = new BookService(sampleBookRepository);
     this.provideTestInput("Domain Driven Design");
     bookService.checkOutBook();
 
@@ -99,9 +104,7 @@ public class BookServiceTest {
 
   @Test
   public void shouldReturnABook() {
-    final SampleBookRepository sampleBookRepository = new SampleBookRepository();
     final StringBuilder expectedOutput = new StringBuilder();
-    final BookService bookService = new BookService(sampleBookRepository);
 
     expectedOutput.append(this.expectedPrintedBooks(sampleBookRepository.getAllAvailable()))
         .append("Please enter the title of the book you want to check out:")
@@ -126,14 +129,12 @@ public class BookServiceTest {
 
   @Test
   public void shouldNotReturnAvailableBook() {
-    final SampleBookRepository sampleBookRepository = new SampleBookRepository();
     final StringBuilder expectedOutput = new StringBuilder();
     expectedOutput.append("Please enter the title of the book you want to return:")
         .append(System.lineSeparator())
         .append("Sorry, that book is not a valid book to return.")
         .append(System.lineSeparator());
     this.provideTestInput("Domain Driven Design");
-    final BookService bookService = new BookService(sampleBookRepository);
     bookService.returnBook();
 
     bookService.printAvailableBookList();
@@ -143,14 +144,12 @@ public class BookServiceTest {
 
   @Test
   public void shouldNotReturnUnValidBook() {
-    final SampleBookRepository sampleBookRepository = new SampleBookRepository();
     final StringBuilder expectedOutput = new StringBuilder();
     expectedOutput.append("Please enter the title of the book you want to return:")
         .append(System.lineSeparator())
         .append("Sorry, that book is not a valid book to return.")
         .append(System.lineSeparator());
     this.provideTestInput("New Domain Driven Design");
-    final BookService bookService = new BookService(sampleBookRepository);
     bookService.returnBook();
 
     bookService.printAvailableBookList();
@@ -167,6 +166,12 @@ public class BookServiceTest {
     final StringBuilder stringBuilder = new StringBuilder();
     books.forEach(book -> stringBuilder.append(book.toString()).append(System.lineSeparator()));
     return stringBuilder.toString();
+  }
+
+  private void logInASampleUser(){
+    User user = new User("012-3456", "test123");
+    user.login("test123");
+    LoggedInUserSingleton.getInstance().setLoggedInUser(Optional.of(user));
   }
 
 }
