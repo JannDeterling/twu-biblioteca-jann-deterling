09fdf4d17be14949cc9abff08ad508d755ce97ff
Jann Deterling <jann.deterling@thoughtworks.com>
2019-10-08 16:10:06 +0200

    [Jann] [2.3] Adds new fields to user

--- src/com/twu/biblioteca/users/User.java
+++ src/com/twu/biblioteca/users/User.java
@@ -3,19 +3,24 @@ package com.twu.biblioteca.users;
 public class User {
 
     private final LibraryNumber libraryNumber;
+    private final String name;
+    private final String phone;
+    private final String email;
     // never store password as string
     private final String password;
     private Boolean isLoggedIn;
 
-    public User(String libraryNumber, String password){
-        this.libraryNumber = new LibraryNumber(libraryNumber);
-        this.password = password;
-        this.isLoggedIn = false;
+    public User(String libraryNumber, String password ,String name, String email, String phone) {
+        this(new LibraryNumber(libraryNumber), password, name, email, phone);
     }
 
-    public User(LibraryNumber libraryNumber, String password){
+    public User(LibraryNumber libraryNumber, String password, String name, String email, String phone) {
         this.libraryNumber = libraryNumber;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
         this.password = password;
+        this.isLoggedIn = false;
     }
 
     public LibraryNumber getLibraryNumber() {
@@ -33,5 +38,28 @@ public class User {
 
     public Boolean isLoggedIn(){
         return this.isLoggedIn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "libraryNumber=" + libraryNumber +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
