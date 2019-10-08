a6ee52b729df0e0af1c9788973bc976f04963471
Jann Deterling <jann.deterling@thoughtworks.com>
2019-10-08 09:41:55 +0200

    [Jann] [2.3] Create test for logging in a user

--- test/com/twu/biblioteca/users/UserTest.java
+++ test/com/twu/biblioteca/users/UserTest.java
@@ -7,20 +7,33 @@ import static org.junit.Assert.assertThat;
 
 public class UserTest {
 
-    private final static String LIBRARY_NUMBER = "000-1234";
-    private final static String PASSWORD = "_test123";
+
 
     @Test
     public void  shouldCreateUser(){
-        User user = new User(LIBRARY_NUMBER, PASSWORD);
+
+        final String libraryNumber = "000-1234";
+        final String password = "_test123";
+        User user = new User(libraryNumber, password);
 
         assertThat(user, is(not(nullValue(User.class))));
         // should set a library number
         assertThat(user.getLibraryNumber(), is(not(nullValue(LibraryNumber.class))));
         // should have the provided libraryNumber
-        assertThat(user.getLibraryNumber().toString(), is(equalTo(LIBRARY_NUMBER)));
+        assertThat(user.getLibraryNumber().toString(), is(equalTo(libraryNumber)));
 
+    }
 
+    @Test
+    public void  shouldLoginUser(){
+
+        final String libraryNumber = "000-1234";
+        final String password = "_test123";
+        User user = new User(libraryNumber, password);
+
+        assertThat(user, is(not(nullValue(User.class))));
+        assertThat(user.login(password), is(true));
+        assertThat(user.isLoggedin(), is(true));
     }
 
 }
