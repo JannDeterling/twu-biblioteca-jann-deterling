ae3aa3627ab68a2b5a1c30e7d8fd7ce713cbde46
Jann Deterling <jann.deterling@thoughtworks.com>
2019-10-08 09:34:16 +0200

    [Jann] [2.3] Adds simple constructors to User

--- src/com/twu/biblioteca/users/User.java
+++ src/com/twu/biblioteca/users/User.java
@@ -2,4 +2,21 @@ package com.twu.biblioteca.users;
 
 public class User {
 
+    private final LibraryNumber libraryNumber;
+    // never store password as string
+    private final String password;
+
+    public User(String libraryNumber, String password){
+        this.libraryNumber = new LibraryNumber(libraryNumber);
+        this.password = password;
+    }
+
+    public User(LibraryNumber libraryNumber, String password){
+        this.libraryNumber = libraryNumber;
+        this.password = password;
+    }
+
+    public LibraryNumber getLibraryNumber() {
+        return libraryNumber;
+    }
 }
