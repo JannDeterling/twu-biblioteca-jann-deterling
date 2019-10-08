2933962521287a8763fbc393e789af4f3fcee3ca
Jann Deterling <jann.deterling@thoughtworks.com>
2019-10-08 09:32:58 +0200

    [Jann] [2.3] Updates LibraryNumber functionality and its test

--- src/com/twu/biblioteca/users/LibraryNumber.java
+++ src/com/twu/biblioteca/users/LibraryNumber.java
@@ -3,11 +3,16 @@ package com.twu.biblioteca.users;
 public class LibraryNumber {
     private final String libraryNumber;
 
-    public LibraryNumber(){
-
-    }
 
     public LibraryNumber(String libraryNumber){
+        assert libraryNumber.length() == 8: "The length of the libraryNumber is not equal 8.";
+        assert libraryNumber.contains("-"): "The libraryNumber is missing a \'-\'";
+        assert libraryNumber.charAt(3) == '-':"The libraryNumber format is wrong should be xxx-xxxx";
+        this.libraryNumber = libraryNumber;
+    }
 
+    @Override
+    public String toString() {
+        return libraryNumber;
     }
 }
--- test/com/twu/biblioteca/users/LibraryNumberTest.java
+++ test/com/twu/biblioteca/users/LibraryNumberTest.java
@@ -8,12 +8,31 @@ import static org.junit.Assert.assertThat;
 public class LibraryNumberTest {
 
     @Test
-    public void shouldGenerateLibraryNumber(){
-        LibraryNumber libraryNumber = new LibraryNumber();
+    public void shouldCreateLibraryNumber(){
+        final String validLibNumber = "012-3456";
+        final LibraryNumber libraryNumber = new LibraryNumber(validLibNumber);
 
         String libNumber = libraryNumber.toString();
         assertThat(libNumber.length(), is(equalTo(8)));
         assertThat(libNumber, containsString("-"));
         assertThat(libNumber.charAt(3), is(equalTo('-')));
+    }
+    
+    @Test(expected = AssertionError.class)
+    public  void shouldNotCreateLibraryNumberToShort(){
+        final String invalidLibNumberToShort = "012-345";
+        new LibraryNumber(invalidLibNumberToShort);
+    }
+
+    @Test(expected = AssertionError.class)
+    public  void shouldNotCreateLibraryNumberNoHyphen(){
+        final String invalidLibNumberNoHyphen = "01234567";
+        new LibraryNumber(invalidLibNumberNoHyphen);
+    }
+
+    @Test(expected = AssertionError.class)
+    public  void shouldNotCreateLibraryNumberWrongHyphen(){
+        final String invalidHyphen = "0123-456";
+        new LibraryNumber(invalidHyphen);
     }
 }
