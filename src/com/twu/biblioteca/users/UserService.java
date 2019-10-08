cea61890c817cf91f7f64d826545bcd77d7ab41f
Jann Deterling <jann.deterling@thoughtworks.com>
2019-10-08 13:41:52 +0200

    [Jann] [2.3] Refactors UserService

--- src/com/twu/biblioteca/users/UserService.java
+++ src/com/twu/biblioteca/users/UserService.java
@@ -6,27 +6,36 @@ import java.util.Scanner;
 public class UserService {
 
     private final SampleUserRepository sampleUserRepository;
+    private final Scanner scanner;
 
     public UserService(SampleUserRepository sampleUserRepository){
+        this.scanner = new Scanner(System.in);
         this.sampleUserRepository = sampleUserRepository;
     }
 
     public void loginUser(){
-        final Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your library number:");
-        final String libraryNumber = scanner.nextLine();
-        Optional<User> userOptional = this.sampleUserRepository.findUserByLibraryNumber(libraryNumber);
+        Optional<User> userOptional = this.getUserInteractiveByLibraryNumber();
         if (userOptional.isPresent()) {
             final User user = userOptional.get();
-            System.out.println("Please enter your password:");
-            final String password = scanner.nextLine();
-            if(user.login(password)){
-                System.out.println("Your are now logged in!");
-            } else  {
-                System.out.println("The entered password is incorrect.");
-            }
+            this.loginUserInteractive(user);
         }else {
             System.out.println("The entered library number is not valid.");
+        }
+    }
+
+    private Optional<User> getUserInteractiveByLibraryNumber() {
+        System.out.println("Please enter your library number:");
+        final String libraryNumber = scanner.nextLine();
+        return this.sampleUserRepository.findUserByLibraryNumber(libraryNumber);
+    }
+
+    private void loginUserInteractive(User user){
+        System.out.println("Please enter your password:");
+        final String password = scanner.nextLine();
+        if(user.login(password)){
+            System.out.println("Your are now logged in!");
+        } else  {
+            System.out.println("The entered password is incorrect.");
         }
     }
 
